06c5e925544e87563d0ff7b95d3be0a497195da8
DTyler0930 <devin.tyler@kellogg.northwestern.edu>
2023-02-21 02:16:46 +0000

    updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by({ "id" => params["id"] })
     @place = Place.find_by({ "id" => @post["place_id"] })
+    @post["place_id"] = @place["id"]
     end
 
  def new
